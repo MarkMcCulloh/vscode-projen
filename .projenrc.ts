a1fdf4da1348f9f76afba241392ac7a98271d1d7
Mark McCulloh <Mark.McCulloh@gmail.com>
2021-12-02 20:46:54 -0500

    wip

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,26 +1,14 @@
-import {
-  FileBase,
-  IgnoreFile,
-  NodePackageManager,
-  TextFile,
-  TypeScriptAppProject,
-} from "projen";
-import { JobPermission } from "projen/lib/github/workflows-model";
-import { Release } from "projen/lib/release";
+import { NodePackageManager } from "projen";
+import { VSCodeExtensionProject } from "./src/project_type";
 
-const project = new TypeScriptAppProject({
-  defaultReleaseBranch: "main",
+const project = new VSCodeExtensionProject({
+  defaultReleaseBranch: "master",
   name: "vscode-projen",
   packageManager: NodePackageManager.NPM,
+  repository: "https://github.com/MarkMcCulloh/vscode-projen.git",
   projenrcTs: true,
   entrypoint: "./lib/extension.js",
-  jestOptions: {
-    jestConfig: {
-      runner: "vscode",
-    },
-  },
-  deps: ["@types/vscode"],
-  devDeps: ["@types/glob", "@vscode/test-electron", "jest-runner-vscode"],
+  devDeps: ["@types/glob"],
   eslintOptions: {
     dirs: ["src"],
     devdirs: ["test"],
@@ -31,136 +19,74 @@ const project = new TypeScriptAppProject({
       lib: ["es2019", "dom"],
     },
   },
-  description: "VSCode extension for Projen",
-});
-
-const release = new Release(project, {
-  branch: "main",
-  task: project.buildTask,
-  versionFile: "package.json",
-  releaseWorkflowName: "Publish",
-});
-
-release.addJobs({
-  publish: {
-    runsOn: "ubuntu-latest",
-    permissions: { contents: JobPermission.READ },
-    steps: [
-      {
-        name: "Publish to Visual Studio Marketplace",
-        uses: "HaaLeo/publish-vscode-extension@v0",
-        with: {
-          pat: "${{ secrets.VS_MARKETPLACE_TOKEN }}",
-          registryUrl: "https://marketplace.visualstudio.com",
+  activationEvents: ["workspaceContains:.projen/**", "onView:projen"],
+  contributes: {
+    viewsContainers: {
+      activitybar: [
+        {
+          id: "projen",
+          title: "Projen",
+          icon: "resources/projen-outline.svg",
         },
-      },
-    ],
-  },
-});
-
-project.jest!.config.globals["ts-jest"] = {
-  tsconfig: "<rootDir>/tsconfig.dev.json",
-};
-
-project.gitignore.addPatterns("out", "*.vsix", ".vscode-test");
-
-project.package.addEngine("vscode", "^1.62.0");
-project.package.addField("displayName", "Projen");
-project.package.addField("icon", "resources/projen.png");
-project.package.addField("preview", true);
-project.package.addField("keywords", ["projen"]);
-project.package.addField("publisher", "mmcculloh");
-
-project.package.addField("activationEvents", [
-  "workspaceContains:.projen/**",
-  "onView:projen",
-]);
-project.package.addField("contributes", {
-  viewsContainers: {
-    activitybar: [
-      {
-        id: "projen",
-        title: "Projen",
-        icon: "resources/projen-outline.svg",
-      },
-    ],
-  },
-  views: {
-    projen: [
-      {
-        id: "projenTasks",
-        name: "Tasks",
-      },
-      {
-        id: "projenDeps",
-        name: "Dependencies",
-      },
-      {
-        id: "projenFiles",
-        name: "Managed Files",
-      },
-      {
-        id: "projenProjects",
-        name: "Available Projects",
-      },
-    ],
-  },
-  commands: [
-    {
-      command: "projen.runProjen",
-      title: "Run Projen",
-      icon: "$(refresh)",
+      ],
     },
-    {
-      command: "projen.runTask",
-      title: "Run Projen Task",
-      icon: "$(play)",
+    views: {
+      projen: [
+        {
+          id: "projenTasks",
+          name: "Tasks",
+        },
+        {
+          id: "projenDeps",
+          name: "Dependencies",
+        },
+        {
+          id: "projenFiles",
+          name: "Managed Files",
+        },
+        {
+          id: "projenProjects",
+          name: "Available Projects",
+        },
+      ],
     },
-    {
-      command: "projen.openProjenRc",
-      title: "Open Projen File",
-      icon: "$(gear)",
-    },
-  ],
-  menus: {
-    "view/title": [
+    commands: [
       {
         command: "projen.runProjen",
-        group: "navigation",
+        title: "Run Projen",
+        icon: "$(refresh)",
+      },
+      {
+        command: "projen.runTask",
+        title: "Run Projen Task",
+        icon: "$(play)",
       },
       {
         command: "projen.openProjenRc",
-        group: "navigation",
+        title: "Open Projen File",
+        icon: "$(gear)",
       },
     ],
+    menus: {
+      "view/title": [
+        {
+          command: "projen.runProjen",
+          group: "navigation",
+        },
+        {
+          command: "projen.openProjenRc",
+          group: "navigation",
+        },
+      ],
+    },
   },
+  displayName: "Projen",
+  icon: "resources/projen.png",
+  releaseToVSMarketplace: true,
+  vscodeVersion: "^1.62.0",
+  description: "VSCode extension for Projen",
+  publisher: "mmcculloh",
+  preview: true,
 });
-
-new TextFile(project, "jest-runner-vscode.config.js", {
-  lines: `\
-// ${FileBase.PROJEN_MARKER}
-
-module.exports = {
-  // Additional arguments to pass to VS Code
-  launchArgs: [
-    '--new-window',
-    '--disable-extensions',
-    '--no-sandbox'
-  ],
-}
-`.split("\n"),
-});
-
-const vscodeIgnore = new IgnoreFile(project, ".vscodeignore");
-vscodeIgnore.addPatterns(
-  ".vscode/**",
-  ".vscode-test/**",
-  "src/**",
-  ".gitignore",
-  "**/tsconfig.json",
-  "**/.eslintrc.json",
-  "**/*.map",
-  "**/*.ts"
-);
 
 project.synth();
---
+++ src/project_type.ts
@@ -0,0 +1,113 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import {
+  FileBase,
+  IgnoreFile,
+  TextFile,
+  TypeScriptAppProject,
+  TypeScriptProjectOptions,
+} from "projen";
+import { JobPermission } from "projen/lib/github/workflows-model";
+import { Release } from "projen/lib/release";
+
+export interface VSCodeExtensionProjectOptions
+  extends TypeScriptProjectOptions {
+  readonly releaseToVSMarketplace?: boolean;
+  readonly vscodeVersion: string;
+  readonly displayName: string;
+  readonly publisher: string;
+  readonly preview: boolean;
+  readonly icon: string;
+  readonly activationEvents: string[];
+  readonly contributes: any;
+  readonly keywords?: string[];
+}
+
+export class VSCodeExtensionProject extends TypeScriptAppProject {
+  public readonly release?: Release;
+  public readonly vscodeIgnore?: IgnoreFile;
+
+  constructor(options: VSCodeExtensionProjectOptions) {
+    super(options);
+
+    this.vscodeIgnore = new IgnoreFile(this, ".vscodeignore");
+    this.vscodeIgnore.addPatterns(
+      ".vscode/**",
+      ".vscode-test/**",
+      "src/**",
+      ".gitignore",
+      "**/tsconfig.json",
+      "**/.eslintrc.json",
+      "**/*.map",
+      "**/*.ts"
+    );
+    this.gitignore.addPatterns("out", "*.vsix", ".vscode-test");
+
+    this.package.addEngine("vscode", options.vscodeVersion);
+    this.package.addField("displayName", options.displayName);
+    this.package.addField("icon", options.icon);
+    this.package.addField("preview", options.preview);
+    this.package.addField("keywords", options.keywords ?? []);
+    this.package.addField("publisher", options.publisher);
+
+    this.package.addField("activationEvents", options.activationEvents);
+    this.package.addField("contributes", options.contributes);
+
+    if (this.jest) {
+      this.addDevDeps(
+        "@vscode/test-electron",
+        "jest-runner-vscode",
+        "@types/vscode"
+      );
+      this.jest.config.runner = "vscode";
+      this.jest.config.globals["ts-jest"] = {
+        tsconfig: `<rootDir>/${this.tsconfigDev.file.path}`,
+      };
+      new TextFile(this, "jest-runner-vscode.config.js", {
+        lines: `\
+// ${FileBase.PROJEN_MARKER}
+
+module.exports = {
+  // Additional arguments to pass to VS Code
+  launchArgs: [
+    '--new-window',
+    '--disable-extensions',
+    '--no-sandbox'
+  ],
+}
+          `.split("\n"),
+      });
+    }
+
+    if (super.release) {
+      this.release = super.release;
+    }
+
+    if (options.releaseToVSMarketplace) {
+      if (!this.release) {
+        this.release = new Release(this, {
+          branch: options.defaultReleaseBranch,
+          task: this.buildTask,
+          versionFile: "package.json",
+          releaseWorkflowName: "Publish",
+        });
+      }
+
+      this.release.addJobs({
+        publish: {
+          runsOn: "ubuntu-latest",
+          permissions: { contents: JobPermission.READ },
+          steps: [
+            {
+              name: "Publish to Visual Studio Marketplace",
+              uses: "HaaLeo/publish-vscode-extension@v0",
+              with: {
+                pat: "${{ secrets.VS_MARKETPLACE_TOKEN }}",
+                registryUrl: "https://marketplace.visualstudio.com",
+              },
+            },
+          ],
+        },
+      });
+    }
+  }
+}
